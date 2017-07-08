a4a5afed221d5323f89c027d075ae8e1be3d451c
rajatyadav3110 <rajatyadav3110@gmail.com>
2017-07-09 02:58:55 +0530

    Created Registration form and Confirm Registration form.
    Date: 2017-07-09

---
+++ app/src/main/java/common/Utility.java
@@ -0,0 +1,21 @@
+package common;
+
+/**
+ * Created by infotech on 09-07-2017.
+ */
+public class Utility {
+
+    public static boolean validateMadatoryString(String value){
+
+        try{
+
+            if(value != null && !value.trim().equals(""))
+                return true;
+            else
+                return false;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+}
